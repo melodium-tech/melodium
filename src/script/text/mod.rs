40a8b4b018f74ab641cef42b9b4f62d8af5a0b75
Quentin VIGNAUD <qvignaud@gmail.com>
2020-05-11 15:30:02 -0400

    Documentation trials.

--- src/script/text/mod.rs
+++ src/script/text/mod.rs
@@ -1,4 +1,10 @@
 
+//! Proceed to basic text parsing and analysis of Mélodium scripts.
+//! 
+//! The main type of this module is `Script`, aimed to handle and build a syntax tree of a script.
+//! Other types may be useful only for extracting specific contents, and are primarily available as branches and leafs of tree built by `Script`.
+//!
+
 pub mod script;
 
 pub mod word;
@@ -10,3 +16,12 @@ pub mod parameter;
 pub mod sequence;
 pub mod treatment;
 pub mod value;
+
+pub use script::Script;
+pub use annotation::Annotation;
+pub use connection::Connection;
+pub use model::Model;
+pub use parameter::Parameter;
+pub use sequence::Sequence;
+pub use treatment::Treatment;
+pub use value::Value;
