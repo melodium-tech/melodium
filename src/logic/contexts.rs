b4a5e8bb9d9b1c114dbc2bd36f209c15ed06c05f
Quentin VIGNAUD <qvignaud@gmail.com>
2020-11-02 12:23:18 +0100

    Adding contexts static collection.

---
+++ src/logic/contexts.rs
@@ -0,0 +1,57 @@
+
+use std::collections::HashMap;
+use std::sync::Arc;
+use super::descriptor::ContextDescriptor;
+use super::descriptor::{DataTypeDescriptor as Data,
+                        DataTypeStructureDescriptor as Structure,
+                        DataTypeTypeDescriptor as Type};
+
+pub struct Contexts {
+    contexts: HashMap<String, Arc<ContextDescriptor>>
+}
+
+impl Contexts {
+
+    fn init() -> Self {
+        let mut contexts = Self {
+            contexts: HashMap::new()
+        };
+
+        contexts.insert(ContextDescriptor::new("Track", vec![
+            ("number", Data::new(Structure::Scalar, Type::Integer)),
+            ("time",  Data::new(Structure::Scalar, Type::Real)),
+        ]));
+
+        contexts.insert(ContextDescriptor::new("File", vec![
+            ("path", Data::new(Structure::Scalar, Type::String)),
+            ("directory",  Data::new(Structure::Scalar, Type::String)),
+            ("name",  Data::new(Structure::Scalar, Type::String)),
+            ("stem",  Data::new(Structure::Scalar, Type::String)),
+            ("extension",  Data::new(Structure::Scalar, Type::String)),
+        ]));
+
+        contexts.insert(ContextDescriptor::new("Signal", vec![
+            ("sampleRate", Data::new(Structure::Scalar, Type::Integer)),
+        ]));
+
+        // Add there existing contexts.
+
+        contexts
+    }
+    
+    fn singleton() -> &'static Self {
+        lazy_static! {
+            static ref SINGLETON: Contexts = Contexts::init();
+        }
+        &SINGLETON
+    }
+
+    fn insert(&mut self, context: ContextDescriptor) {
+        self.contexts.insert(context.name().to_string(), Arc::new(context));
+    }
+
+    pub fn get(name: &str) -> Option<&'static Arc<ContextDescriptor>> {
+
+        Self::singleton().contexts.get(name)
+    }
+}
--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -8,6 +8,7 @@ pub mod builder;
 pub mod collection;
 pub mod collection_pool;
 pub mod connections;
+pub mod contexts;
 pub mod descriptor;
 pub mod designer;
 pub mod error;
