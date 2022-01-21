c49b5c10acd194d4bf44d67c2598d37e29d2ef0c
Quentin VIGNAUD <Quentin VIGNAUD>
2022-01-21 09:47:08 +0100

    Making some implementation trials for better transmitters.

--- src/executive/transmitter.rs
+++ src/executive/transmitter.rs
@@ -1,7 +1,9 @@
 
+use std::sync::Mutex;
 pub use async_std::channel::Sender;
 pub use async_std::channel::Receiver;
 pub use async_std::channel::{bounded, unbounded};
+use std::sync::atomic::{AtomicBool, Ordering};
 
 #[derive(Debug, Clone)]
 pub enum Transmitter {
@@ -46,4 +48,143 @@ pub enum Transmitter {
     VecChar(Sender<Vec<char>>),
     VecString(Sender<Vec<String>>),
 
+}
+
+const BUFFER_LIMIT: usize = 2usize.pow(20);
+
+pub type SendResult = Result<(), TransmissionError>;
+pub type RecvResult<T> = Result<T, TransmissionError>;
+
+pub enum TransmissionError {
+    NoReceiver,
+    EverythingClosed,
+}
+
+pub struct SendTransmitter<T> {
+    senders: Mutex<Vec<Sender<Vec<T>>>>,
+    buffer: Mutex<Vec<T>>,
+
+    has_receivers: AtomicBool,
+}
+
+impl<T: Clone> SendTransmitter<T> {
+
+    pub fn new() -> Self {
+        Self {
+            senders: Mutex::new(Vec::new()),
+            buffer: Mutex::new(Vec::with_capacity(BUFFER_LIMIT)),
+            has_receivers: AtomicBool::new(false),
+        }
+    }
+
+    pub fn add_transmitter(&self, transmitter: &RecvTransmitter<T>) {
+
+        let sender = transmitter.get_sender();
+        self.senders.lock().unwrap().push(sender);
+
+        self.has_receivers.store(true, Ordering::Relaxed);
+    }
+
+    pub async fn send(&self, data: T) -> SendResult {
+
+        if !self.has_receivers.load(Ordering::Relaxed) {
+            return Err(TransmissionError::NoReceiver)
+        }
+        else
+        {
+            let mut buffer = self.buffer.lock().unwrap();
+            buffer.push(data);
+        }
+        
+        self.check_send().await
+    }
+
+    pub async fn send_multiple(&self, data: Vec<T>) -> SendResult {
+
+        if !self.has_receivers.load(Ordering::Relaxed) {
+            return Err(TransmissionError::NoReceiver)
+        }
+        else
+        {
+            let mut buffer = self.buffer.lock().unwrap();
+            buffer.extend(data);
+        }
+        
+        self.check_send().await
+    }
+
+    async fn check_send(&self) -> SendResult {
+
+        let mut buffer = self.buffer.lock().unwrap();
+
+        if buffer.len() >= BUFFER_LIMIT {
+
+            let mut statuses = Vec::new();
+            for sender in self.senders.lock().unwrap().iter() {
+                statuses.push(
+                    match sender.send(buffer.clone()).await {
+                        Ok(()) => true,
+                        Err(_) => false,
+                    }
+                );
+            };
+
+            let status = if let Some(_) = statuses.iter().find(|s| **s) {
+                Ok(())
+            }
+            else {
+                Err(TransmissionError::EverythingClosed)
+            };
+
+            buffer.clear();
+
+            return status;
+        }
+
+        Ok(())
+    }
+
+    pub fn close(&self) {
+        self.senders.lock().unwrap().iter().for_each(|s| { s.close(); } );
+    }
+}
+
+trait SenderGetter<T> {
+    fn get_sender(&self) -> Sender<Vec<T>>;
+}
+
+pub struct RecvTransmitter<T> {
+    receiver: Receiver<Vec<T>>,
+    sender: Sender<Vec<T>>,
+}
+
+impl<T: Clone> RecvTransmitter<T> {
+
+    pub fn new() -> Self {
+        let (sender, receiver) = unbounded();
+
+        Self {
+            sender,
+            receiver,
+        }
+    }
+
+    pub async fn receive_multiple(&self) -> RecvResult<Vec<T>> {
+        
+        match self.receiver.recv().await {
+            Ok(v) => Ok(v),
+            Err(_) => Err(TransmissionError::EverythingClosed),
+        }
+    }
+
+    pub fn close(&self) {
+        self.receiver.close();
+    }
+}
+
+impl<T> SenderGetter<T> for RecvTransmitter<T> {
+
+    fn get_sender(&self) -> Sender<Vec<T>> {
+        self.sender.clone()
+    }
 }
