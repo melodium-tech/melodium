e928f8c81d078a428ef7d83f0ca7226e9f77d7de
Quentin VIGNAUD <qvignaud@gmail.com>
2022-01-24 22:06:06 +0100

    Fixing documentation

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,15 @@
 //! 
 //! Mélodium is a tool and language for manipulation of large amount of data, using the definition of treatments that applies on data through connections, with a track approach that makes any script higly scalable and implicitly parallelizable.
 //! 
-//! Mélodium is **under development** and still being defined and improved to become fully operationnal. Its project is hosted on GitLab at <https://gitlab.com/qvignaud/melodium-rust>.
+//! For more exhaustive explanations, please refer to [the Mélodium Language book](https://melodium.gitlab.io/book/).
+//! 
+//! Mélodium is _under development_ and continously being defined and improved. The development documentation is available at <https://melodium.gitlab.io/melodium/melodium/>, and the core reference at <https://melodium.gitlab.io/melodium/reference/>.
 //! 
 //! ## Origin
 //! 
-//! Mélodium were first developed during research in signal analysis and musical information retrieval, in need of a tool to manage large amount of records and easily write experimentations, without being concerned of underlying technical operations. It has been presented in [this thesis](https://www.researchgate.net/publication/344327676_Detection_et_classification_des_notes_d'une_piste_audio_musicale)(in French).
+//! Mélodium were first developed during research in signal analysis and musical information retrieval, in need of a tool to manage large amount of records and easily write experimentations, without being concerned of underlying technical operations. It has been presented in [this thesis](https://www.researchgate.net/publication/344327676_Detection_et_classification_des_notes_d'une_piste_audio_musicale) (in French).
 //! 
-//! The first implementation was in C++ and ran well on high performance computers, such as those of Compute Canada. That tool appeared to be really useful, and the concepts used within its configuration language to deserve more attention. This first experimental design is still available at <https://gitlab.com/qvignaud/Melodium>.
+//! The first implementation was in C++ and ran well on high performance computers, such as those of Compute Canada. That tool appeared to be really useful, and the concepts used within its configuration language to deserve more attention.
 //! 
 //! The current project is the continuation of that work, rewritten from ground in Rust, and redesigned with a general approach of massively multithreaded data flows in mind.
 
