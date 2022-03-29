42e237b98d89f29db2d4e33fe23623ee29d39a39
Quentin VIGNAUD <qvignaud@gmail.com>
2022-03-29 18:53:22 +0200

    Fixing audio tests.

--- melodium-tests/audio/test_audio.sh
+++ melodium-tests/audio/test_audio.sh
@@ -4,14 +4,4 @@ set -e
 
 "$MELODIUM" --stdlib /tmp/fake_std audio_wave.mel
 
-sha256sum reencoded_*
-echo hexdump -C ./reencoded_i16.wav
-hexdump -C ./reencoded_i16.wav
-
-echo hexdump -C ./reencoded_i24.wav
-hexdump -C ./reencoded_i24.wav
-
-echo hexdump -C ./reencoded_f32.wav
-hexdump -C ./reencoded_f32.wav
-
 sha256sum -c reencoded.sha256sums
