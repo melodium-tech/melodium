75f717e0973b22f3f304e030fbee62b5e1c2f002
Quentin VIGNAUD <qvignaud@gmail.com>
2022-03-29 14:36:21 +0200

    Adding wave format encoder.

--- src/core/audio/encoding/wave/mod.rs
+++ src/core/audio/encoding/wave/mod.rs
@@ -4,6 +4,7 @@ use crate::core::prelude::*;
 pub fn register(mut c: &mut CollectionPool) {
 
     decode::register(&mut c);
+    encode::register(&mut c);
 }
 
 treatment!(decode,
@@ -124,3 +125,50 @@ treatment!(decode,
         ResultStatus::Ok
     }
 );
+
+treatment!(encode,
+    core_identifier!("audio","encoding","wave";"EncodeWave"),
+    models![],
+    treatment_sources![],
+    parameters![],
+    inputs![
+        input!("signal",Scalar,F32,Stream)
+    ],
+    outputs![
+        output!("data",Vector,Byte,Block)
+    ],
+    host {
+
+        use hound::*;
+
+        let input = host.get_input("signal");
+        let output = host.get_output("data");
+
+        let mut cursor_writer = std::io::Cursor::new(Vec::new());
+        {
+            let mut wav_writer = WavWriter::new(
+                &mut cursor_writer,
+                WavSpec {
+                    channels: 1,
+                    sample_rate: 44100,
+                    bits_per_sample: 32,
+                    sample_format: SampleFormat::Float,
+                }
+            ).unwrap();
+    
+            while let Ok(signal_chunks) = input.recv_f32().await {
+    
+                for sample in signal_chunks {
+                    wav_writer.write_sample(sample).unwrap();
+                }
+            }
+        }
+
+        let data_written = cursor_writer.into_inner();
+
+        let _ = output.send_vec_byte(data_written).await;
+    
+        ResultStatus::Ok
+    }
+);
+
