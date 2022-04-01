532a6e878070279a2570ca7906e442a598a00b4b
Quentin VIGNAUD <qvignaud@gmail.com>
2022-04-01 14:44:46 +0200

    Adding real wave decoding model.

--- src/core/audio/encoding/wave/mod.rs
+++ src/core/audio/encoding/wave/mod.rs
@@ -1,15 +1,20 @@
 
 use crate::core::prelude::*;
 
+mod decoder;
+
 pub fn register(mut c: &mut CollectionPool) {
 
+    decoder::register(&mut c);
     decode::register(&mut c);
     encode::register(&mut c);
 }
 
 treatment!(decode,
     core_identifier!("audio","encoding","wave";"DecodeWave"),
-    models![],
+    models![
+        ("decoder", crate::core::audio::encoding::wave::decoder::WaveDecoderModel::descriptor())
+    ],
     treatment_sources![],
     parameters![],
     inputs![
@@ -20,106 +25,18 @@ treatment!(decode,
     ],
     host {
 
-        use hound::*;
+        use crate::core::audio::encoding::wave::decoder::WaveDecoderModel;
 
         let input = host.get_input("data");
-        let output = host.get_output("signal");
+        let decoder = Arc::clone(&host.get_model("decoder")).downcast_arc::<WaveDecoderModel>().unwrap();
     
-        while let Ok(data) = input.recv_vec_byte().await {
+        if let Ok(data) = input.recv_vec_byte().await {
 
             // We're taking a block
             let data = data.get(0).unwrap();
             input.close();
 
-            let mut reader = WavReader::new(data.as_slice()).unwrap();
-
-            let spec = reader.spec();
-
-            match (spec.sample_format, spec.bits_per_sample) {
-                (SampleFormat::Float, 32) => {
-                    for sample in reader.samples::<f32>() {
-                        if let Ok(sample) = sample {
-                            ok_or_break!(output.send_f32(sample).await);
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                },
-                (SampleFormat::Int, 8) => {
-                    for sample in reader.samples::<i8>() {
-                        if let Ok(sample) = sample {
-                            // Please keep linear conversion explicit
-                            let sample = (
-                                (sample as f32 - i8::MIN as f32)
-                                / (i8::MAX as f32 - i8::MIN as f32)
-                            )
-                            * (1.0 - -1.0) - 1.0;
-
-                            ok_or_break!(output.send_f32(sample).await);
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                },
-                (SampleFormat::Int, 16) => {
-                    for sample in reader.samples::<i16>() {
-                        if let Ok(sample) = sample {
-                            // Please keep linear conversion explicit
-                            let sample = (
-                                (sample as f32 - i16::MIN as f32)
-                                / (i16::MAX as f32 - i16::MIN as f32)
-                            )
-                            * (1.0 - -1.0) - 1.0;
-                            
-                            ok_or_break!(output.send_f32(sample).await);
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                },
-                (SampleFormat::Int, 24) => {
-                    for sample in reader.samples::<i32>() {
-                        if let Ok(sample) = sample {
-                            // Please keep linear conversion explicit
-                            let sample = (
-                                (sample as f32 - -8_388_608 as f32)
-                                / (8_388_607 as f32 - -8_388_608 as f32)
-                            )
-                            * (1.0 - -1.0) - 1.0;
-                            
-                            ok_or_break!(output.send_f32(sample).await);
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                },
-                (SampleFormat::Int, 32) => {
-                    for sample in reader.samples::<i32>() {
-                        if let Ok(sample) = sample {
-                            // Please keep linear conversion explicit
-                            let sample = (
-                                (sample as f32 - i32::MIN as f32)
-                                / (i32::MAX as f32 - i32::MIN as f32)
-                            )
-                            * (1.0 - -1.0) - 1.0;
-                            
-                            ok_or_break!(output.send_f32(sample).await);
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                },
-                _ => ()
-            }
-
-            
-
-            //ok_or_break!(output.send_multiple_f32(signal).await);
+            decoder.add_to_decode(data.to_vec()).await;
         }
     
         ResultStatus::Ok
