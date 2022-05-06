08595c93a828ad077f65a402ac26fddac2d5c9b5
Quentin VIGNAUD <qvignaud@gmail.com>
2022-05-06 12:08:14 +0200

    Adding the core implementation to convert string in specific encoding.

--- src/core/text/bytes_to_string.rs
+++ src/core/text/bytes_to_string.rs
@@ -46,7 +46,53 @@ treatment!(byte_to_string,
     }
 );
 
+treatment!(string_to_byte,
+    core_identifier!("text";"EncodeBytes"),
+    models![],
+    treatment_sources![],
+    parameters![
+        parameter!("encoding",Var,Scalar,String,Some(Value::String("utf-8".to_string())))
+    ],
+    inputs![
+        input!("value",Scalar,String,Stream)
+    ],
+    outputs![
+        output!("data",Scalar,Byte,Stream)
+    ],
+    host {
+        let input = host.get_input("value");
+        let output = host.get_output("data");
+
+        let encoding = encoding_rs::Encoding::for_label(
+            host.get_parameter("encoding").string().as_bytes()
+        ).unwrap_or(encoding_rs::UTF_8);
+        let mut encoder = encoding.new_encoder();
+
+        'main: loop {
+
+            if let Ok(strings) = input.recv_string().await {
+                for string in strings {
+
+                    let mut result = Vec::new();
+                    let _ = encoder.encode_from_utf8_to_vec(&string, &mut result, false);
+                    ok_or_break!('main, output.send_multiple_byte(result).await);
+                }
+            }
+            else {
+                // Here we finish the encoding as required by encoding_rs
+                let mut result = Vec::new();
+                let _ = encoder.encode_from_utf8_to_vec(&String::new(), &mut result, true);
+                ok_or_break!(output.send_multiple_byte(result).await);
+                break;
+            }
+        }
+    
+        ResultStatus::Ok
+    }
+);
+
 pub fn register(mut c: &mut CollectionPool) {
 
     byte_to_string::register(&mut c);
+    string_to_byte::register(&mut c);
 }
