1451df13abc4dde372edf20c78580bc55796a9d1
Quentin VIGNAUD <qvignaud@gmail.com>
2022-06-29 16:53:20 +0200

    Changing StreamBlock base behavior.

--- src/core/flow/block_to_stream.rs
+++ src/core/flow/block_to_stream.rs
@@ -2,17 +2,17 @@
 use crate::core::prelude::*;
 
 macro_rules! impl_BlockToStream {
-    ($mod:ident, $mel_name:expr, $mel_type:ident, $recv_func:ident, $send_func:ident) => {
+    ($mod:ident, $mel_name:expr, $mel_struct:ident, $mel_type:ident, $recv_func:ident, $send_func:ident) => {
         treatment!($mod,
             core_identifier!("flow";$mel_name),
             models![],
             treatment_sources![],
             parameters![],
             inputs![
-                input!("data",Vector,$mel_type,Block)
+                input!("data",$mel_struct,$mel_type,Block)
             ],
             outputs![
-                output!("data",Scalar,$mel_type,Stream)
+                output!("data",$mel_struct,$mel_type,Stream)
             ],
             host {
                 let input = host.get_input("data");
@@ -28,25 +28,61 @@ macro_rules! impl_BlockToStream {
     }
 }
 
-impl_BlockToStream!(block_vec_void_to_stream, "BlockVecVoidToStreamVoid", Void, recv_vec_void, send_multiple_void);
-impl_BlockToStream!(block_vec_u8_to_stream, "BlockVecU8ToStreamU8", U8, recv_vec_u8, send_multiple_u8);
-impl_BlockToStream!(block_vec_u16_to_stream, "BlockVecU16ToStreamU16", U16, recv_vec_u16, send_multiple_u16);
-impl_BlockToStream!(block_vec_u32_to_stream, "BlockVecU32ToStreamU32", U32, recv_vec_u32, send_multiple_u32);
-impl_BlockToStream!(block_vec_u64_to_stream, "BlockVecU64ToStreamU64", U64, recv_vec_u64, send_multiple_u64);
-impl_BlockToStream!(block_vec_u128_to_stream, "BlockVecU128ToStreamU128", U128, recv_vec_u128, send_multiple_u128);
-impl_BlockToStream!(block_vec_i8_to_stream, "BlockVecI8ToStreamI8", I8, recv_vec_i8, send_multiple_i8);
-impl_BlockToStream!(block_vec_i16_to_stream, "BlockVecI16ToStreamI16", I16, recv_vec_i16, send_multiple_i16);
-impl_BlockToStream!(block_vec_i32_to_stream, "BlockVecI32ToStreamI32", I32, recv_vec_i32, send_multiple_i32);
-impl_BlockToStream!(block_vec_i64_to_stream, "BlockVecI64ToStreamI64", I64, recv_vec_i64, send_multiple_i64);
-impl_BlockToStream!(block_vec_i128_to_stream, "BlockVecI128ToStreamI128", I128, recv_vec_i128, send_multiple_i128);
-impl_BlockToStream!(block_vec_f32_to_stream, "BlockVecF32ToStreamF32", F32, recv_vec_f32, send_multiple_f32);
-impl_BlockToStream!(block_vec_f64_to_stream, "BlockVecF64ToStreamF64", F64, recv_vec_f64, send_multiple_f64);
-impl_BlockToStream!(block_vec_bool_to_stream, "BlockVecBoolToStreamBool", Bool, recv_vec_bool, send_multiple_bool);
-impl_BlockToStream!(block_vec_byte_to_stream, "BlockVecByteToStreamByte", Byte, recv_vec_byte, send_multiple_byte);
-impl_BlockToStream!(block_vec_char_to_stream, "BlockVecCharToStreamChar", Char, recv_vec_char, send_multiple_char);
-impl_BlockToStream!(block_vec_string_to_stream, "BlockVecStringToStreamString", String, recv_vec_string, send_multiple_string);
+impl_BlockToStream!(block_void_to_stream, "BlockVoidToStream", Scalar, Void, recv_void, send_void);
+impl_BlockToStream!(block_u8_to_stream, "BlockU8ToStream", Scalar, U8, recv_u8, send_u8);
+impl_BlockToStream!(block_u16_to_stream, "BlockU16ToStream", Scalar, U16, recv_u16, send_u16);
+impl_BlockToStream!(block_u32_to_stream, "BlockU32ToStream", Scalar, U32, recv_u32, send_u32);
+impl_BlockToStream!(block_u64_to_stream, "BlockU64ToStream", Scalar, U64, recv_u64, send_u64);
+impl_BlockToStream!(block_u128_to_stream, "BlockU128ToStream", Scalar, U128, recv_u128, send_u128);
+impl_BlockToStream!(block_i8_to_stream, "BlockI8ToStream", Scalar, I8, recv_i8, send_i8);
+impl_BlockToStream!(block_i16_to_stream, "BlockI16ToStream", Scalar, I16, recv_i16, send_i16);
+impl_BlockToStream!(block_i32_to_stream, "BlockI32ToStream", Scalar, I32, recv_i32, send_i32);
+impl_BlockToStream!(block_i64_to_stream, "BlockI64ToStream", Scalar, I64, recv_i64, send_i64);
+impl_BlockToStream!(block_i128_to_stream, "BlockI128ToStream", Scalar, I128, recv_i128, send_i128);
+impl_BlockToStream!(block_f32_to_stream, "BlockF32ToStream", Scalar, F32, recv_f32, send_f32);
+impl_BlockToStream!(block_f64_to_stream, "BlockF64ToStream", Scalar, F64, recv_f64, send_f64);
+impl_BlockToStream!(block_bool_to_stream, "BlockBoolToStream", Scalar, Bool, recv_bool, send_bool);
+impl_BlockToStream!(block_byte_to_stream, "BlockByteToStream", Scalar, Byte, recv_byte, send_byte);
+impl_BlockToStream!(block_char_to_stream, "BlockCharToStream", Scalar, Char, recv_char, send_char);
+impl_BlockToStream!(block_string_to_stream, "BlockStringToStream", Scalar, String, recv_string, send_string);
+
+impl_BlockToStream!(block_vec_void_to_stream, "BlockVecVoidToStream", Vector, Void, recv_vec_void, send_vec_void);
+impl_BlockToStream!(block_vec_u8_to_stream, "BlockVecU8ToStream", Vector, U8, recv_vec_u8, send_vec_u8);
+impl_BlockToStream!(block_vec_u16_to_stream, "BlockVecU16ToStream", Vector, U16, recv_vec_u16, send_vec_u16);
+impl_BlockToStream!(block_vec_u32_to_stream, "BlockVecU32ToStream", Vector, U32, recv_vec_u32, send_vec_u32);
+impl_BlockToStream!(block_vec_u64_to_stream, "BlockVecU64ToStream", Vector, U64, recv_vec_u64, send_vec_u64);
+impl_BlockToStream!(block_vec_u128_to_stream, "BlockVecU128ToStream", Vector, U128, recv_vec_u128, send_vec_u128);
+impl_BlockToStream!(block_vec_i8_to_stream, "BlockVecI8ToStream", Vector, I8, recv_vec_i8, send_vec_i8);
+impl_BlockToStream!(block_vec_i16_to_stream, "BlockVecI16ToStream", Vector, I16, recv_vec_i16, send_vec_i16);
+impl_BlockToStream!(block_vec_i32_to_stream, "BlockVecI32ToStream", Vector, I32, recv_vec_i32, send_vec_i32);
+impl_BlockToStream!(block_vec_i64_to_stream, "BlockVecI64ToStream", Vector, I64, recv_vec_i64, send_vec_i64);
+impl_BlockToStream!(block_vec_i128_to_stream, "BlockVecI128ToStream", Vector, I128, recv_vec_i128, send_vec_i128);
+impl_BlockToStream!(block_vec_f32_to_stream, "BlockVecF32ToStream", Vector, F32, recv_vec_f32, send_vec_f32);
+impl_BlockToStream!(block_vec_f64_to_stream, "BlockVecF64ToStream", Vector, F64, recv_vec_f64, send_vec_f64);
+impl_BlockToStream!(block_vec_bool_to_stream, "BlockVecBoolToStream", Vector, Bool, recv_vec_bool, send_vec_bool);
+impl_BlockToStream!(block_vec_byte_to_stream, "BlockVecByteToStream", Vector, Byte, recv_vec_byte, send_vec_byte);
+impl_BlockToStream!(block_vec_char_to_stream, "BlockVecCharToStream", Vector, Char, recv_vec_char, send_vec_char);
+impl_BlockToStream!(block_vec_string_to_stream, "BlockVecStringToStream", Vector, String, recv_vec_string, send_vec_string);
 
 pub fn register(mut c: &mut CollectionPool) {
+
+    block_void_to_stream::register(&mut c);
+    block_u8_to_stream::register(&mut c);
+    block_u16_to_stream::register(&mut c);
+    block_u32_to_stream::register(&mut c);
+    block_u64_to_stream::register(&mut c);
+    block_u128_to_stream::register(&mut c);
+    block_i8_to_stream::register(&mut c);
+    block_i16_to_stream::register(&mut c);
+    block_i32_to_stream::register(&mut c);
+    block_i64_to_stream::register(&mut c);
+    block_i128_to_stream::register(&mut c);
+    block_f32_to_stream::register(&mut c);
+    block_f64_to_stream::register(&mut c);
+    block_bool_to_stream::register(&mut c);
+    block_byte_to_stream::register(&mut c);
+    block_char_to_stream::register(&mut c);
+    block_string_to_stream::register(&mut c);
 
     block_vec_void_to_stream::register(&mut c);
     block_vec_u8_to_stream::register(&mut c);
