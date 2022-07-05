a6d1c4d4f7f81f184439c7d7a0bf80778cf4b29a
Quentin VIGNAUD <qvignaud@gmail.com>
2022-07-05 14:00:50 +0200

    Removing cast & conversion of vectors from core treatments, but to-void ones. All of these are now provided through standard library.

--- src/core/conversion/mod.rs
+++ src/core/conversion/mod.rs
@@ -1,9 +1,7 @@
 
 pub mod scalar_float_to_integer;
-pub mod vector_float_to_integer;
 pub mod scalar_float;
 pub mod scalar_to_string;
-pub mod vector_to_string;
 pub mod scalar_to_byte;
 pub mod scalar_to_void;
 pub mod vector_to_void;
