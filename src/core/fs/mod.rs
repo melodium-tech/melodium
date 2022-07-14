e7516a8f341b55141afdea1288adc03d0bbd4906
Quentin VIGNAUD <qvignaud@gmail.com>
2022-07-14 15:20:03 +0200

    Removing obsolete core fs elements.

--- src/core/fs/mod.rs
+++ src/core/fs/mod.rs
@@ -1,19 +1,10 @@
 
-
-pub mod direct;
-
-pub mod files;
-pub mod read_files;
-pub mod write_files;
-
 pub mod read;
 pub mod write;
 
 use crate::core::prelude::*;
 
 pub fn register(mut c: &mut CollectionPool) {
-
-    direct::register(&mut c);
 
     read::register(&mut c);
     write::register(&mut c);
