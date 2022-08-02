c34172e3440850a2e96d0135ca14be134b644301
Quentin VIGNAUD <qvignaud@gmail.com>
2022-08-02 23:14:24 +0200

    Continuing svg trials.

--- src/graph/draw.rs
+++ src/graph/draw.rs
@@ -17,10 +17,11 @@ pub fn draw(sequence: Arc<RwLock<SequenceDesigner>>) -> String {
         }
     }
 
-    result.push_str(&format!(r#"<svg height="30" width="{}">"#, (max_level + 1) * 10));
+    result.push_str(&format!(r#"<svg xmlns="http://www.w3.org/2000/svg" height="1200" width="{}"><style>{}</style>"#, (max_level + 1) * 100, include_str!("style.css")));
 
-    for (name, treatment) in sequence.treatments() {
-        result.push_str(&format!(r#"<text x="{}" y="15">{}</text>"#, treatment.read().unwrap().level() * 10, name));
+    for (_name, t) in sequence.treatments() {
+        //result.push_str(&format!(r#"<text x="{}" y="15">{}</text>"#, treatment.read().unwrap().level() * 10, name));
+        result.push_str(&treatment(t, t.read().unwrap().level() as u64 * 100, 0));
     }
 
     result.push_str(&format!("</svg>"));
@@ -29,23 +30,25 @@ pub fn draw(sequence: Arc<RwLock<SequenceDesigner>>) -> String {
 }
 
 
-fn treatment(treatment: Arc<RwLock<TreatmentDesigner>>) -> String {
+fn treatment(treatment: &Arc<RwLock<TreatmentDesigner>>, x: u64, y: u64) -> String {
 
     let treatment = treatment.read().unwrap();
-    let max_iop = vec![
-        treatment.descriptor().inputs().len(),
-        treatment.descriptor().outputs().len(),
+    let descriptor = treatment.descriptor();
+    let max_iop = *vec![
+        descriptor.inputs().len(),
+        descriptor.outputs().len(),
         treatment.parameters().len(),
         ].iter().max().unwrap();
 
     let width = 100;
-    let height = (*max_iop as u64 + 2) * 20;
+    let height = (max_iop as u64 + 2) * 20;
 
     let mut result = String::new();
 
-    result.push_str(&format!(r#"<g id="{}">"#, treatment.name()));
+    result.push_str(&format!(r#"<g id="{}" transform="translate({} {})">"#, treatment.name(), x, y));
 
-    result.push_str(&format!(r#"<rect width="{}" height="{}" rx="15" />"#, width, height));
+    result.push_str(&format!(r#"<rect class="treatment" width="{}" height="{}" rx="15" />"#, width, height));
+    result.push_str(&format!(r#"<text class="treatment-name" text-anchor="middle" x="{}" y="20">{}</text><text class="treatment-type" text-anchor="middle" x="{}" y="35">{}</text>"#, width / 2, treatment.name(), width / 2, descriptor.identifier().name()));
 
     result.push_str("</g>");
 
