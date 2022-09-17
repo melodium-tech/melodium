63001e5ca0342568b109c5676ad82fda214df964
Quentin VIGNAUD <qvignaud@gmail.com>
2022-09-17 15:53:10 +0200

    Fixing std lib inclusion creation.

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 
 fn main() {
 
-    let mut std_content = String::from("");
+    let mut std_content = String::from("{");
 
     for entry in glob::glob("std/**/*.mel").unwrap() {
         match entry {
@@ -15,13 +15,15 @@ fn main() {
                     },
                 };
 
-                std_content.push_str(&format!(r#"content.insert("{}", include_str!("{}"));\n"#, relative_path.to_str().unwrap(), absolute_path.to_str().unwrap()));
+                std_content.push_str(&format!(r#"content.insert("{}", include_str!("{}"));"#, relative_path.to_str().unwrap(), absolute_path.to_str().unwrap()));
             }
             Err(e) => {
                 panic!("{}", e)
             }
         }
     }
+
+    std_content.push('}');
 
     std::fs::write(std::path::Path::new(std::env::var("OUT_DIR").as_ref().unwrap()).join("stdlib.rs"), std_content).unwrap();
 }
