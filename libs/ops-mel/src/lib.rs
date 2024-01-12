50fcf11f611b25b92ba30aecf1bf70719231f240
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2024-01-12 17:04:51 +0100

    Implementing Order trait treatments/functions

--- libs/ops-mel/src/lib.rs
+++ libs/ops-mel/src/lib.rs
@@ -147,4 +147,85 @@ pub async fn lower_equal() {
     }
 }
 
+/// Gives the minimum value between `a` and `b`.
+#[mel_function(
+    generic T (Order)
+)]
+pub fn min(a: T, b: T) -> T {
+    a.order_min(&b)
+}
+
+/// Stream the minimum value between `a` and `b`.
+#[mel_treatment(
+    generic T (Order)
+    input a Stream<T>
+    input b Stream<T>
+    output min Stream<T>
+)]
+pub async fn min() {
+    while let (Ok(a), Ok(b)) = (a.recv_one().await, b.recv_one().await) {
+        check!(min.send_one(a.order_min(&b)).await)
+    }
+}
+
+/// Gives the maximum value between `a` and `b`.
+#[mel_function(
+    generic T (Order)
+)]
+pub fn max(a: T, b: T) -> T {
+    a.order_max(&b)
+}
+
+/// Stream the maximum value between `a` and `b`.
+#[mel_treatment(
+    generic T (Order)
+    input a Stream<T>
+    input b Stream<T>
+    output max Stream<T>
+)]
+pub async fn max() {
+    while let (Ok(a), Ok(b)) = (a.recv_one().await, b.recv_one().await) {
+        check!(max.send_one(a.order_max(&b)).await)
+    }
+}
+
+/// Restrain value between `min` and `max`.
+///
+/// Value is returned if it is in the [`min`,`max`] interval.
+/// If value is smaller `min` is returned, or if it greater `max` is returned.
+#[mel_function(
+    generic T (Order)
+)]
+pub fn clamp(value: T, min: T, max: T) -> T {
+    value.order_clamp(&min, &max)
+}
+
+/// Stream value restrained between `min` and `max`.
+///
+/// Value is streamed if it is in the [`min`,`max`] interval.
+/// If value is smaller `min` is sent, or if it greater `max` is sent.
+#[mel_treatment(
+    generic T (Order)
+    input value Stream<T>
+    output clamped Stream<T>
+)]
+pub async fn clamp(min: T, max: T) {
+    while let Ok(values) = value
+        .recv_many()
+        .await
+        .map(|values| Into::<VecDeque<Value>>::into(values))
+    {
+        check!(
+            clamped
+                .send_many(TransmissionValue::Other(
+                    values
+                        .into_iter()
+                        .map(|val| val.order_clamp(&min, &max))
+                        .collect()
+                ))
+                .await
+        )
+    }
+}
+
 mel_package!();
