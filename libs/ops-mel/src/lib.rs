9756c49805e6db1b775d1f21c7e2d21c8b4b29fd
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2024-01-12 16:41:29 +0100

    Implementing PartialEq/PartialOrd traits

--- libs/ops-mel/src/lib.rs
+++ libs/ops-mel/src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
 
+use melodium_core::*;
 use melodium_macro::{check, mel_function, mel_package, mel_treatment};
 
 pub mod bool;
@@ -22,43 +23,127 @@ pub mod u8;
 
 /// Return whether `a` is equal to `b`
 #[mel_function(
-    generic T ()
+    generic T (PartialEquality)
 )]
 pub fn equal(a: T, b: T) -> bool {
-    a == b
+    a.partial_equality_eq(&b)
 }
 
 /// Return whether `a` is different `b`
 #[mel_function(
-    generic T ()
+    generic T (PartialEquality)
 )]
 pub fn not_equal(a: T, b: T) -> bool {
-    a != b
+    a.partial_equality_ne(&b)
 }
 
 /// Determine whether `a` is equal to `b`
 #[mel_treatment(
-    generic T ()
+    generic T (PartialEquality)
     input a Stream<T>
     input b Stream<T>
     output result Stream<bool>
 )]
 pub async fn equal() {
     while let (Ok(a), Ok(b)) = (a.recv_one().await, b.recv_one().await) {
-        check!(result.send_one((a == b).into()).await)
+        check!(result.send_one(a.partial_equality_eq(&b).into()).await)
     }
 }
 
 /// Determine whether `a` is different from `b`
 #[mel_treatment(
-    generic T ()
+    generic T (PartialEquality)
     input a Stream<T>
     input b Stream<T>
     output result Stream<bool>
 )]
 pub async fn not_equal() {
     while let (Ok(a), Ok(b)) = (a.recv_one().await, b.recv_one().await) {
-        check!(result.send_one((a != b).into()).await)
+        check!(result.send_one(a.partial_equality_ne(&b).into()).await)
+    }
+}
+
+/// Tells whether `a` is strictly greater than `b`.
+#[mel_function(
+    generic T (PartialOrder)
+)]
+pub fn gt(a: T, b: T) -> bool {
+    a.partial_order_gt(&b)
+}
+
+/// Determine whether `a` is strictly greater than `b`
+#[mel_treatment(
+    generic T (PartialOrder)
+    input a Stream<T>
+    input b Stream<T>
+    output is Stream<bool>
+)]
+pub async fn greater_than() {
+    while let (Ok(a), Ok(b)) = (a.recv_one().await, b.recv_one().await) {
+        check!(is.send_one(a.partial_order_gt(&b).into()).await)
+    }
+}
+
+/// Tells whether `a` is greater or equal to `b`.
+#[mel_function(
+    generic T (PartialOrder)
+)]
+pub fn ge(a: T, b: T) -> bool {
+    a.partial_order_ge(&b)
+}
+
+/// Determine whether `a` is greater or equal to `b`
+#[mel_treatment(
+    generic T (PartialOrder)
+    input a Stream<T>
+    input b Stream<T>
+    output is Stream<bool>
+)]
+pub async fn greater_equal() {
+    while let (Ok(a), Ok(b)) = (a.recv_one().await, b.recv_one().await) {
+        check!(is.send_one(a.partial_order_ge(&b).into()).await)
+    }
+}
+
+/// Tells whether `a` is strictly lower than `b`.
+#[mel_function(
+    generic T (PartialOrder)
+)]
+pub fn lt(a: T, b: T) -> bool {
+    a.partial_order_lt(&b)
+}
+
+/// Determine whether `a` is strictly lower than `b`
+#[mel_treatment(
+    generic T (PartialOrder)
+    input a Stream<T>
+    input b Stream<T>
+    output is Stream<bool>
+)]
+pub async fn lower_than() {
+    while let (Ok(a), Ok(b)) = (a.recv_one().await, b.recv_one().await) {
+        check!(is.send_one(a.partial_order_lt(&b).into()).await)
+    }
+}
+
+/// Tells whether `a` is lower or equal to `b`.
+#[mel_function(
+    generic T (PartialOrder)
+)]
+pub fn le(a: T, b: T) -> bool {
+    a.partial_order_le(&b)
+}
+
+/// Determine whether `a` is lower or equal to `b`
+#[mel_treatment(
+    generic T (PartialOrder)
+    input a Stream<T>
+    input b Stream<T>
+    output is Stream<bool>
+)]
+pub async fn lower_equal() {
+    while let (Ok(a), Ok(b)) = (a.recv_one().await, b.recv_one().await) {
+        check!(is.send_one(a.partial_order_le(&b).into()).await)
     }
 }
 
