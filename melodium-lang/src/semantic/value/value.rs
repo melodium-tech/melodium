a8b778554ee9ddd3812a2dfa28273a8502638af0
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2024-01-16 16:00:03 +0100

    Fixing generic function invocation bug

--- melodium-lang/src/semantic/value/value.rs
+++ melodium-lang/src/semantic/value/value.rs
@@ -356,9 +356,13 @@ impl Value {
                         if let Some(rc_param) = borrowed_func.parameters.get(i) {
                             let borrowed_param = rc_param.read().unwrap();
 
+                            let described_type = desc_param
+                                .described_type()
+                                .as_defined(&generics)
+                                .unwrap_or_else(|| desc_param.described_type().clone());
+
                             if let Some(param) = result.merge_degrade_failure(
-                                borrowed_param
-                                    .make_designed_value(designer, desc_param.described_type()),
+                                borrowed_param.make_designed_value(designer, &described_type),
                             ) {
                                 params.push(param);
                             }
