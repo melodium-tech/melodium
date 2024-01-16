19d275aadd71627d5bbdb3eb2c84d231d2d017dc
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2024-01-16 10:13:50 +0100

    Moving text functions list

--- libs/text-mel/src/lib.rs
+++ libs/text-mel/src/lib.rs
@@ -7,4 +7,18 @@ pub mod compare;
 pub mod compose;
 pub mod convert;
 
+    // Studied functions not given to 'text' trait but should be implemented in this lib
+    /*fn text_contains(&self, other: &Value) -> Value;
+    fn text_ends_with(&self, other: &Value) -> Value;
+    fn text_is_ascii(&self) -> Value;
+    fn text_len(&self) -> Value;
+    fn text_replace(&self, from: &Value, to: &Value) -> Value;
+    fn text_to_lowercase(&self) -> Value;
+    fn text_to_uppercase(&self) -> Value;
+    fn text_trim(&self) -> Value;
+    fn text_trim_start(&self) -> Value;
+    fn text_trim_start_matches(&self, trim: &Value) -> Value;
+    fn text_trim_end(&self) -> Value;
+    fn text_trim_end_matches(&self, trim: &Value) -> Value;*/
+
 mel_package!();
--- melodium-common/src/executive/data_traits.rs
+++ melodium-common/src/executive/data_traits.rs
@@ -90,20 +90,6 @@ pub trait DataTrait: Sized {
     fn float_to_degrees(&self) -> Self;
     fn float_to_radians(&self) -> Self;
 
-    // text (may not be a trait as everything is only for 'string' type)
-    /*fn text_contains(&self, other: &Value) -> Value;
-    fn text_ends_with(&self, other: &Value) -> Value;
-    fn text_is_ascii(&self) -> Value;
-    fn text_len(&self) -> Value;
-    fn text_replace(&self, from: &Value, to: &Value) -> Value;
-    fn text_to_lowercase(&self) -> Value;
-    fn text_to_uppercase(&self) -> Value;
-    fn text_trim(&self) -> Value;
-    fn text_trim_start(&self) -> Value;
-    fn text_trim_start_matches(&self, trim: &Value) -> Value;
-    fn text_trim_end(&self) -> Value;
-    fn text_trim_end_matches(&self, trim: &Value) -> Value;*/
-
     fn partial_equality_eq(&self, other: &Self) -> bool;
     fn partial_equality_ne(&self, other: &Self) -> bool;
 
