a7cc9550e002df5d019192fe7862f7f7d05bb9c4
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2024-03-12 15:16:40 +0100

    Fixing SQL tester

--- tests/testers/sql/src/main.rs
+++ tests/testers/sql/src/main.rs
@@ -6,7 +6,7 @@ const EXEC_ERROR_FILENAME: &str = "execution_error";
 const SUCCESS_FILENAME: &str = "success_affected";
 
 fn main() {
-    if env::var("CI").is_some() && cfg!(target_os = "windows") {
+    if env::var("CI").is_ok() && cfg!(target_os = "windows") {
         // On CI for Windows, for now, we skip SQL tests as we have no acceptable solution
         // to get a working and available DBMS to make tests with (see tests.yml file).
         exit(0);
