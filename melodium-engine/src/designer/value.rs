ead37518db88a3e11a314e9e0880171ad7025e01
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2024-03-15 17:01:44 +0100

    Fixing scope vs. parent generics for function call

--- melodium-engine/src/designer/value.rs
+++ melodium-engine/src/designer/value.rs
@@ -242,7 +242,7 @@ impl Value {
                     .merge_degrade_failure(function_instanciation.check_function_return(parameters))
                 {
                     if !described_type.is_compatible(
-                        &parent_generics.read().unwrap(),
+                        &scope_generics.read().unwrap(),
                         &sub_return_type,
                         &generics,
                     ) {
