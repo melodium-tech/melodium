abb882ad41c00ce9cb711844cb3895ebd4c553db
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2024-11-07 18:39:56 +0100

    Adding README and CHANGELOG files for new crates

--- melodium-distributed/src/lib.rs
+++ melodium-distributed/src/lib.rs
@@ -1,3 +1,6 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc = include_str!("../README.md")]
+
 mod error;
 mod listen;
 mod messages;
--- melodium-sharing/src/lib.rs
+++ melodium-sharing/src/lib.rs
@@ -1,3 +1,6 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc = include_str!("../README.md")]
+
 mod attribute;
 mod collection;
 mod connection_design;
