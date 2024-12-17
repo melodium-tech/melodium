96ff769396a7b360260ba0007f36f128276c8a7a
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2024-12-17 15:59:05 +0100

    Fixing tests

--- libs/work-mel/src/access.rs
+++ libs/work-mel/src/access.rs
@@ -10,15 +10,9 @@ use uuid::Uuid;
 pub struct Access(pub api::CommonAccess);
 
 #[mel_function]
-pub fn new_access(
-    ipv4: Ipv4,
-    ipv6: Ipv6,
-    port: u16,
-    remote_key: string,
-    self_key: string,
-) -> Access {
+pub fn new_access(ip: Vec<Ip>, port: u16, remote_key: string, self_key: string) -> Access {
     Access(api::CommonAccess {
-        addresses: vec![ipv6.0.into(), ipv4.0.into()],
+        addresses: ip.into_iter().map(|ip| ip.0).collect(),
         port,
         remote_key: Uuid::from_str(&remote_key).unwrap_or_default(),
         self_key: Uuid::from_str(&self_key).unwrap_or_default(),
