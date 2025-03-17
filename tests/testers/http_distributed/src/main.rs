3b1a0b2b653b8947224c8f71853d3866c7eeda4c
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2025-03-17 16:29:40 +0100

    Changing port for http_distributed

--- tests/testers/http_distributed/src/main.rs
+++ tests/testers/http_distributed/src/main.rs
@@ -39,7 +39,7 @@ fn main() {
 
     for trial in 0..3 {
         sleep(Duration::from_millis(500));
-        match ureq::post("http://localhost:8080/hello")
+        match ureq::post("http://localhost:28015/hello")
             .config()
             .timeout_global(Some(Duration::from_secs(1)))
             .build()
