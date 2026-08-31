b911040d3a896548a99a1449a8a6c2abe60c11e1
Quentin VIGNAUD <quentin.vignaud@melodium.tech>
2026-08-31 15:23:51 +0000

    Resolve "Chunked, size-capped wire framing for the distribution protocol"

--- melodium-distribution/src/lib.rs
+++ melodium-distribution/src/lib.rs
@@ -2,15 +2,17 @@
 #![doc = include_str!("../README.md")]
 
 mod error;
+mod framing;
 mod listen;
 mod messages;
 mod protocol;
 
 pub use error::{DistributionError, DistributionResult};
+pub use framing::{chunk_raw_values, max_batch_chunk_bytes, max_frame_bytes};
 pub use listen::{launch_listen, launch_listen_localcert, launch_listen_unsecure};
 pub use messages::*;
 pub use protocol::{Error, Protocol};
 
 use melodium_common::descriptor::Version;
 
-pub static VERSION: Version = Version::new(0, 1, 0);
+pub static VERSION: Version = Version::new(0, 2, 0);
